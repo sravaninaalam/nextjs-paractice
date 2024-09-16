b4320484a91f2b6cee3ee0d1247ede21e76bb403
sravaninaalam <129977519+sravaninaalam@users.noreply.github.com>
2024-09-16 13:32:24 +0530

    useRouter

--- components/Dynamicproduct.js
+++ components/Dynamicproduct.js
@@ -1,15 +1,18 @@
 'use client'
 
-import { useState } from "react"
+import {useState } from "react"
 import Link from "next/link"
+import { useRouter } from "next/navigation"
 function Dynamicproduct() {
     const [num,setNum]=useState(0)
+    const router=useRouter()
   return (
     <div>
        <input type="number" className="border border-black" value={num} onChange={(e)=>setNum(e.target.value)} />
        
        <br></br>
-       <Link href={`/products/${num}`} className='text-blue-700'>goto {num}</Link>
+       {/* <Link href={`/products/${num}`} className='text-blue-700'>goto {num}</Link> */}
+       <button onClick={()=>router.push(`/products/${num}`)}>GoTO {num}</button>
     </div>
   )
 }
